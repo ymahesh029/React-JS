f1e2a1896ef4e5a4718700d7f7a8110c7e285ac0
cleverteam21 <cleverteam21@gmail.com>
2022-09-05 12:54:26 +0530

    class Components

---
+++ rjsfc/src/Components/ClassCompo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Person extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return <div>class componensts</div>;
+  }
+}
+
+export default Person;
